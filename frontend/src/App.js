ab88cfa82a60cc8e912ef848f15cb149c88eef0d
piraniya <rayarlaxman@example.com>
2024-03-22 15:16:24 +0530

    added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 
+import About from './components/About';
+import Login from './components/Login';
+import SignUp from './components/SignUp';
+import Product from './components/Product';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import AdminPannel from '../src/components/Admin/AdminPannel';
+import Home from '../src/components/Home/Home';
+import PlantCard from './components/Plant/PlantCard';
+import PlantDeatils from './components/Plant/PlantDeatils';
+import Fertilizer from './components/Plant/Fertilizer';
+import FertilizerDetails from './components/Plant/FertilizerDeatils';
+
+import Cart from './components/Cart'; 
+import { useEffect } from "react";
+
+
+
+
 function App() {
+
+  const [cartItems, setCartItems] = useState([]);
+  const [isAdminRoute, setIsAdminRoute] = useState(false);
+ 
+
+  useEffect(() => {
+    // Check if the current route is an admin route
+    setIsAdminRoute(window.location.pathname.startsWith('/admin'));
+  }, []);
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      
+        <Router>
+        {!isAdminRoute && <Header cartItems={cartItems} />} 
+          <Routes>
+            <Route path="/" element={<About />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<SignUp />} />
+            <Route path="/PlantCard" element={<PlantCard />} />
+            <Route path="/admin/*" element={<AdminPannel />} />
+            <Route path="/home/*" element={<Home />} />
+            <Route path="/product" element={<Product />} />
+            <Route path="/plants/:id" element={<PlantDeatils />} />
+            <Route path="/fertilizer" element={<Fertilizer />} />
+            {/* <Route path="/fertilizers/:id" element={<FertilizerDetails />} /> */}
+            <Route path='/viewdetails/:id' element={<FertilizerDetails cartItems={cartItems} setCartItems={setCartItems} />} />
+            <Route path="/cart" element={<Cart cartItems={cartItems} setCartItems={setCartItems} />} />
+           
+          </Routes>
+          {!isAdminRoute && <Footer/> }
+        </Router>
+        
+      
     </div>
   );
 }
