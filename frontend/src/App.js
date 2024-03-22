4276ab0c4d68a400a298497a0b03432606e65364
piraniya <rayarlaxman@example.com>
2024-03-22 15:56:37 +0530

    added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,7 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 import React, { useState } from 'react';
+
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 
